96e0f54e021c5d64486dacfcddf5f656257f5635
cegeh1456 <rezaupper1456@gmail.com>
2026-05-31 12:24:19 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 //  Isi dengan kredensial Supabase kamu di sini
 // =====================================================
 
-const SUPABASE_URL = 'zokhradghyebmfdwnixq';
+const SUPABASE_URL = 'https://zokhradghyebmfdwnixq.supabase.co/rest/v1/';
 // Contoh: 'https://abcdefghij.supabase.co'
 
 const SUPABASE_ANON_KEY = 'GANTI_DENGAN_SUPABASE_ANON_KEY_KAMU';
